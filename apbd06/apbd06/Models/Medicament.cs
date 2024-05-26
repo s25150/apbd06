51f6d3de0249c664baad1606d70e6931334158fe
Oliwia Leszczyńska <s25150@pjwstk.edu.pl>
2024-05-26 15:07:47 +0200

    v1.1

---
+++ apbd06/apbd06/Models/Medicament.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace apbd06.Models;
+
+public class Medicament
+{
+    [Key]
+    public int IdMedicament { get; set; }
+    [MaxLength(100)]
+    public string Name { get; set; }
+    [MaxLength(100)]
+    public string Description { get; set; }
+    [MaxLength(100)]
+    public string Type { get; set; }
+}
